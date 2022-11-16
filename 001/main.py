a813583e700f2d6dddcc68cb2a3ba4ae7cb9dc17
Maciej <47892486+MacioSzekTV@users.noreply.github.com>
2022-11-16 14:31:09 +0100

    Create main.py

---
+++ 001/main.py
@@ -0,0 +1,9 @@
+x = input("Podaj 1 liczbę: ")
+y = input("Podaj 2 liczbę: ")
+
+if x==y:
+  print("Liczby są równe")
+if x>y:
+  print("Pierwsza liczba jest większa od drugiej")
+if y>x:
+  print("Druga liczba jest większa od pierwszej")
