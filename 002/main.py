a1784c823e684fb3dbda88d8775ff63644e29fa0
Maciej <47892486+MacioSzekTV@users.noreply.github.com>
2022-11-16 14:33:21 +0100

    Create main.py

---
+++ 002/main.py
@@ -0,0 +1,8 @@
+x = input("Podaj liczbę: ")
+
+if int(x)==0:
+  print("Liczba jest równa zero")
+if int(x)>0:
+  print("Liczba jest większa od zera")
+if 0>int(x):
+  print("Liczba jest mniejsza od zera")
