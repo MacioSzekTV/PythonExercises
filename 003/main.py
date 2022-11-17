b9148f0585a616bb0bddec8dc6de1f228efeada8
Maciej <47892486+MacioSzekTV@users.noreply.github.com>
2022-11-17 15:01:09 +0100

    Update main.py

--- 003/main.py
+++ 003/main.py
@@ -4,18 +4,14 @@ z = input("Podaj trzecią liczbę: ")
 
 boki = [int(x),int(y),int(z)]
 
-size = len(boki)
+bokis = sorted(boki)
+print(bokis)
 
-for i in range(size):
-  for j in range(0, size-i-1):
-    if boki[i] > boki[j+1]:
-      boki[j], boki[j+1] = boki[j+1], boki[j]
-
-if boki[0]+boki[1]>boki[2]:
+if bokis[0]+bokis[1]>bokis[2]:
   print("Z takich boków można zbudować trójkąt")
 
-if boki[0]+boki[1]<boki[2]:
+if bokis[0]+bokis[1]<bokis[2]:
   print("Nie można zbudować trójkątu")
 
-if boki[0]+boki[1]==boki[2]:
+if bokis[0]+bokis[1]==bokis[2]:
   print("Nie można zbudować trójkątu")
